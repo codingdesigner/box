3fa6eb5f4a503ae729e6e37f24824f04b279554a
Mason Wendell <mason@thecodingdesigner.com>
2014-05-31 01:13:52 -0400

    some new slides

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
   $("#main-title h1").lettering('words');
   $("#main-title h2").lettering('words');
   $("#main-title p").lettering('words');
+
+  $("#contact h1").lettering('words').children('span').lettering();;
 });
